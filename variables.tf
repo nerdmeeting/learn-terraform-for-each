fb16b9cc9e0ce67a4abf93d49a8e76652f9a0b54
Nerd Nathan <nathan@nerdmeeting.com>
2024-12-26 21:02:48 -0700

    Set version to 0.1.0.

--- variables.tf
+++ variables.tf
@@ -1,3 +1,9 @@
+variable "project_version" {
+  description = "Version of the project"
+  type = string
+  default = "0.1.0"
+}
+
 variable "aws_region" {
   description = "AWS region for all resources."
   type        = string

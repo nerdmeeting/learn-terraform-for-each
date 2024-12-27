03127d77f2853beec8e7fa721b66b4818b47258e
Nathan Bills <nathan@nerdmeeting.com>
2024-12-26 20:48:52 -0700

    Replace the resource "aws_instance" "app" and data "aws_ami" "amazon_linux" blocks with a reference to the aws-instance module.

--- main.tf
+++ main.tf
@@ -94,37 +94,17 @@ module "elb_http" {
   }
 }
 
-data "aws_ami" "amazon_linux" {
-  most_recent = true
-  owners      = ["amazon"]
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-}
-
-resource "aws_instance" "app" {
-  count = 2
-
-  ami           = data.aws_ami.amazon_linux.id
-  instance_type = var.instance_type
-
-  subnet_id              = module.vpc.private_subnets[0]
-  vpc_security_group_ids = [module.app_security_group.security_group_id]
-
-  user_data = <<-EOF
-    #!/bin/bash
-    sudo yum update -y
-    sudo yum install httpd -y
-    sudo systemctl enable httpd
-    sudo systemctl start httpd
-    echo "<html><body><div>Hello, world!</div></body></html>" > /var/www/html/index.html
-    EOF
-
-  tags = {
-    Terraform   = "true"
-    Project     = var.project_name
-    Environment = var.environment
-  }
+module "ec2_instances" {
+  source = "./modules/aws-instance"
+  depends_on = [module.vpc]
+  
+  for_each = var.project
+  
+  instance_count = each.value.instances_per_subnet * length(module.vpc[each.key].private_subnets)
+  instance_type = each.value.instance_type
+  subnet_ids = module.vpc[each.key].private_subnets[*]
+  security_group_ids = [module.app_security_group[each.key].security_group_id]
+  
+  project_name = each.key
+  environment = each.value.environment
 }

f90017fc90488ad52bffc175bf718c35e6ca0793
Nathan Bills <nathan@nerdmeeting.com>
2024-12-26 20:26:58 -0700

    Remove the now-unnecessary variables due to the new project variable.

--- variables.tf
+++ variables.tf
@@ -4,36 +4,6 @@ variable "aws_region" {
   default     = "us-east-2"
 }
 
-variable "project_name" {
-  description = "Name of the project. Used in resource names and tags."
-  type        = string
-  default     = "client-webapp"
-}
-
-variable "environment" {
-  description = "Value of the 'Environment' tag."
-  type        = string
-  default     = "dev"
-}
-
-variable "public_subnets_per_vpc" {
-  description = "Number of public subnets. Maximum of 16."
-  type        = number
-  default     = 2
-}
-
-variable "private_subnets_per_vpc" {
-  description = "Number of private subnets. Maximum of 16."
-  type        = number
-  default     = 2
-}
-
-variable "instance_type" {
-  description = "Type of EC2 instance to use."
-  type        = string
-  default     = "t2.micro"
-}
-
 variable "vpc_cidr_block" {
   description = "CIDR block for VPC."
   type        = string
